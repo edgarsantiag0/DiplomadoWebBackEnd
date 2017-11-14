ecd026067075920f0249cb24cb9ddbb1712d3282
Edgar Santiago <itt.edgarstgo@gmail.com>
2017-11-13 22:17:25 -0400

    Fix clases

---
+++ DiplomadoWebBackEnd/Clase2/Sobrecarga.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomadoWebBackEnd.Clase1
+{
+    class Sobrecarga
+    {
+
+        static void Metodo(int x)
+        {
+            Console.WriteLine("Metodo(int x)");
+        }
+
+        static void Metodo(string y)
+        {
+            Console.WriteLine("Metodo(string y)");
+        }
+
+        static void Metodo(double x, int y)
+        {
+            Console.WriteLine("Metodo(double x, int y)");
+        }
+    }
+}
